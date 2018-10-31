994e752b5316a6be91eee1499f45390925aff728
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2018-10-31 15:28:04 +0100

    OPENEUROPA-1316: Apply requested changes and corrections.

--- tests/FunctionalJavascript/MediaRemoteVideoTest.php
+++ tests/FunctionalJavascript/MediaRemoteVideoTest.php
@@ -22,15 +22,24 @@ class MediaRemoteVideoTest extends WebDriverTestBase {
   ];
 
   /**
-   * Video urls and names.
-   *
-   * @var array
+   * Data provider for testRemoteVideoMedia().
    */
-  public $videos = [
-    'https://www.youtube.com/watch?v=1-g73ty9v04' => 'Energy, let\'s save it!',
-    'https://vimeo.com/7073899' => 'Drupal Rap Video - Schipulcon09',
-    'http://www.dailymotion.com/video/x6pa0tr' => 'European Commission Fines Google',
-  ];
+  public function providerRemoteVideoMedia() {
+    return [
+      'Youtube' => [
+        'https://www.youtube.com/watch?v=1-g73ty9v04',
+        'Energy, let\'s save it!',
+      ],
+      'Vimeo' => [
+        'https://vimeo.com/7073899',
+        'Drupal Rap Video - Schipulcon09',
+      ],
+      'Dailymotion' => [
+        'http://www.dailymotion.com/video/x6pa0tr',
+        'European Commission Fines Google',
+      ],
+    ];
+  }
 
   /**
    * {@inheritdoc}
@@ -51,44 +60,46 @@ class MediaRemoteVideoTest extends WebDriverTestBase {
 
   /**
    * Test the creation of Media Remote video entity and reuse on the Demo node.
+   *
+   * @param string $video_url
+   *   Video URL.
+   * @param string $video_name
+   *   Name of Video.
+   *
+   * @dataProvider providerRemoteVideoMedia
    */
-  public function testCreateRemoteVideoMedia(): void {
+  public function testRemoteVideoMedia($video_url, $video_name): void {
     $session = $this->getSession();
     $page = $session->getPage();
     $assert_session = $this->assertSession();
-    $id = 1;
-    foreach ($this->videos as $video_url => $video_name) {
-      // Create a media item.
-      $this->drupalGet("media/add/remote_video");
-      $page->fillField("oe_media_oembed_video[0][value]", $video_url);
-      $page->pressButton('Save');
-      $this->createScreenshot($this->root . '/screenshot.jpg');
-      $assert_session->addressEquals('/media/' . $id);
 
-      // Create a node with attached media.
-      $this->drupalGet("node/add/oe_media_demo");
-      $page->fillField("title[0][value]", 'My Node');
-      $autocomplete_field = $page->findField('field_oe_demo_remote_video_media[0][target_id]');
-      $autocomplete_field->setValue($video_name);
-      $this->getSession()->getDriver()->keyDown($autocomplete_field->getXpath(), ' ');
-      $this->assertSession()->waitOnAutocomplete();
-      $this->getSession()->getDriver()->click($page->find('css', '.ui-autocomplete li')->getXpath());
-      $page->pressButton('Save');
-      $assert_session->addressEquals('/node/' . $id);
+    // Create a media item.
+    $this->drupalGet("media/add/remote_video");
+    $page->fillField("oe_media_oembed_video[0][value]", $video_url);
+    $page->pressButton('Save');
+    $assert_session->addressEquals('/media/1');
 
-      // Ensure the iframe exists and that its src attribute contains a coherent
-      // URL with the query parameters we expect.
-      $iframe_url = $assert_session->elementExists('css', 'iframe')->getAttribute('src');
-      $iframe_url = parse_url($iframe_url);
-      $this->assertStringEndsWith('/media/oembed', $iframe_url['path']);
-      $this->assertNotEmpty($iframe_url['query']);
-      $query = [];
-      parse_str($iframe_url['query'], $query);
-      $this->assertSame($video_url, $query['url']);
-      $this->assertNotEmpty($query['hash']);
+    // Create a node with attached media.
+    $this->drupalGet("node/add/oe_media_demo");
+    $page->fillField("title[0][value]", 'My Node');
+    $autocomplete_field = $page->findField('field_oe_demo_remote_video_media[0][target_id]');
+    $autocomplete_field->setValue($video_name);
+    $this->getSession()->getDriver()->keyDown($autocomplete_field->getXpath(), ' ');
+    $this->assertSession()->waitOnAutocomplete();
+    $this->getSession()->getDriver()->click($page->find('css', '.ui-autocomplete li')->getXpath());
+    $page->pressButton('Save');
+    $assert_session->addressEquals('/node/1');
 
-      $id++;
-    }
+    // Ensure the iframe exists and that its src attribute contains a coherent
+    // URL with the query parameters we expect.
+    $iframe_url = $assert_session->elementExists('css', 'iframe')->getAttribute('src');
+    $iframe_url = parse_url($iframe_url);
+    $this->assertStringEndsWith('/media/oembed', $iframe_url['path']);
+    $this->assertNotEmpty($iframe_url['query']);
+    $query = [];
+    parse_str($iframe_url['query'], $query);
+    $this->assertSame($video_url, $query['url']);
+    $this->assertNotEmpty($query['hash']);
   }
 
 }
