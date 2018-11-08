dffab2cbe5db2df65262cc77b84f8848657c4647
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2018-11-08 13:49:26 +0100

    OPENEUROPA-1311: Try to fix failed drone tests related to permissions.

--- tests/FunctionalJavascript/MediaEntityBrowserTest.php
+++ tests/FunctionalJavascript/MediaEntityBrowserTest.php
@@ -35,10 +35,6 @@ class MediaEntityBrowserTest extends WebDriverTestBase {
     ]);
 
     $this->drupalLogin($editor);
-    // There are permission issues with the Docker container
-    // so we need to manually change the permissions to allow file uploads.
-    exec('chmod -R 777 ' . $this->publicFilesDirectory);
-    exec('chmod -R 777 ' . $this->tempFilesDirectory);
   }
 
   /**
@@ -50,12 +46,11 @@ class MediaEntityBrowserTest extends WebDriverTestBase {
    *   The contents of the file.
    */
   public function createMediaImageEntity(string $name, string $file_source): void {
-    $file = file_save_data(file_get_contents($file_source), 'public://' . basename($file_source));
+    $file = file_save_data(file_get_contents($file_source), 'public://' . $name);
     /** @var \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager */
     $entityTypeManager = $this->container->get('entity_type.manager');
     $entityTypeManager->getStorage('media')->create([
       'bundle' => 'image',
-      'name' => $name,
       'oe_media_image' => [
         'target_id' => $file->id(),
       ],
@@ -66,12 +61,11 @@ class MediaEntityBrowserTest extends WebDriverTestBase {
    * Test the media entity browser.
    */
   public function testMediaBrowser(): void {
-    $image_name = 'My Image 1';
     $filename = 'example_1.jpeg';
     $path = drupal_get_path('module', 'oe_media');
     $file_source = $this->root . '/' . $path . '/tests/fixtures/' . $filename;
 
-    $this->createMediaImageEntity($image_name, $file_source);
+    $this->createMediaImageEntity($filename, $file_source);
 
     // Select media image though entity browser.
     $this->drupalGet('node/add/oe_media_demo');
@@ -82,7 +76,6 @@ class MediaEntityBrowserTest extends WebDriverTestBase {
     // Go to modal window.
     $this->assertSession()->assertWaitOnAjaxRequest();
     $this->getSession()->switchToIFrame('entity_browser_iframe_media_entity_browser');
-    $this->getSession()->wait(3000);
     $this->assertSession()->assertWaitOnAjaxRequest();
     $iframe_page = $this->getSession()->getPage();
     $iframe_page->hasSelect('Publishing status');
