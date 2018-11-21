bfb44c690e4dfa01bef9f43a0c122938bb3d46a7
upchuk <danny@webomelette.com>
2018-11-21 08:45:04 +0100

    OPENEUROPA-1325: Minor test improvements.

--- tests/Behat/DrupalContext.php
+++ tests/Behat/DrupalContext.php
@@ -7,21 +7,24 @@ namespace Drupal\Tests\oe_media\Behat;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 
 /**
- * Class DrupalContext.
+ * The main Drupal context.
  */
 class DrupalContext extends RawDrupalContext {
 
   /**
-   * Click on drupal fiedlset form element.
+   * Clicks on a fieldset form element.
    *
-   * @Given I click fieldset :field of entity browser widget
+   * @param string $field
+   *   The name of the fieldset.
+   *
+   * @Given I click the fieldset :field
    */
-  public function iClickFieldset(string $field): void {
+  public function assertClickFieldset(string $field): void {
     $this->getSession()->getPage()->find('named', ['link_or_button', $field])->click();
   }
 
   /**
-   * Switching to the iframe.
+   * Switches to the iframe of the Demo entity browser.
    *
    * @Then I should see entity browser modal window
    */
@@ -30,15 +33,36 @@ class DrupalContext extends RawDrupalContext {
   }
 
   /**
-   * Select required media from galary.
+   * Selects required media entity from entity browser.
+   *
+   * @param string $name
+   *   The name of the media.
    *
    * @When I select the :media_name media entity in the entity browser modal window
    */
-  public function iSelectMediaInEntityBrowser(string $media_name): void {
+  public function iSelectMediaInEntityBrowser(string $name): void {
     $xpath = "//div[@class and contains(concat(' ', normalize-space(@class), ' '), ' views-row ')]";
-    $xpath .= "[.//div[@class and contains(concat(' ', normalize-space(@class), ' '), ' views-field-name ')][contains(string(.), '$media_name')]]";
+    $xpath .= "[.//div[@class and contains(concat(' ', normalize-space(@class), ' '), ' views-field-name ')][contains(string(.), '$name')]]";
     $xpath .= "//input[@type='checkbox']";
     $this->getSession()->getPage()->find('xpath', $xpath)->check();
+  }
+
+  /**
+   * Step that deletes a media entity.
+   *
+   * @param string $title
+   *   The media title.
+   *
+   * @Then I remove the media :title
+   */
+  public function removeMediaEntity(string $title): void {
+    $media = \Drupal::entityTypeManager()->getStorage('media')->loadByProperties(['name' => $title]);
+    if (!$media) {
+      throw new \Exception(sprintf('The media named "%s" does not exist', $title));
+    }
+
+    $media = reset($media);
+    $media->delete();
   }
 
 }
--- tests/features/media-file.feature
+++ tests/features/media-file.feature
@@ -2,15 +2,15 @@
 Feature: Media document bundle.
   In order to be able to showcase the media and entity browser features for managing files
   As a site editor
-  I want to see the entity browser widget for adding and reusing document media entities.
+  I want to be able to work with File based Media entities
 
   @javascript
-  Scenario: The node adding form should contain entity browser widget with possibility to add new and reuse existing files.
+  Scenario: The entity browser should allow the selection and creation of new File Media entities
     Given I am logged in as a user with the "create oe_media_demo content,create document media,access media_entity_browser entity browser pages" permissions
     
     When I visit "node/add/oe_media_demo"
     And I fill in "Title" with "Media demo"
-    And I click fieldset "Media browser field" of entity browser widget
+    And I click the fieldset "Media browser field"
     And I press the "Select entities" button
     Then I should see entity browser modal window
     When I click "Add File"
@@ -18,15 +18,17 @@ Feature: Media document bundle.
     And I attach the file "sample.pdf" to "File"
     And I press the "Save entity" button
     And I press the "Save" button
-    Then I should see "sample.pdf"
+    Then I should see the link "sample.pdf"
 
     When I visit "node/add/oe_media_demo"
     And I fill in "Title" with "Media demo"
-    And I click fieldset "Media browser field" of entity browser widget
+    And I click the fieldset "Media browser field"
     And I press the "Select entities" button
     Then I should see entity browser modal window
     When I click "View"
     And I select the "Media document" media entity in the entity browser modal window
     And I press the "Select entities" button
     And I press the "Save" button
-    Then I should see "sample.pdf"
+    Then I should see the link "sample.pdf"
+    # Cleanup of the media entity.
+    And I remove the media "Media document"
