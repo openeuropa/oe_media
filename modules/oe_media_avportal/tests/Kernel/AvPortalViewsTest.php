5aa0ff806f17eb1be3617b52f23dfa79be3eee32
Hernani Borges de Freitas <hernani@Hernanis-Air.lan>
2019-02-05 13:13:43 +0000

    OPENEUROPA-1558: Added tests for both resources.

--- modules/oe_media_avportal/tests/Kernel/AvPortalViewsTest.php
+++ modules/oe_media_avportal/tests/Kernel/AvPortalViewsTest.php
@@ -48,7 +48,7 @@ class AvPortalViewsTest extends KernelTestBase {
   }
 
   /**
-   * Tests the View that uses the AV Portal query plugin.
+   * Tests the View that uses the AV Portal query plugin with video.
    */
   public function testDefaultAvPortalVideoViews(): void {
     $view = Views::getView('av_portal_test');
@@ -133,6 +133,50 @@ class AvPortalViewsTest extends KernelTestBase {
     $row = $view->result[0];
     $this->assertEquals('P-039300/00-05', $row->ref);
     $this->assertEquals('Participation of Jean-Claude Juncker, President of the EC, at debate on the future of Europe, with Members of the European Parliament ', $row->title);
+  }
+
+  /**
+   * Tests the View that uses the AV Portal query plugin with both types.
+   */
+  public function testDefaultAvPortalBothViews(): void {
+    $view = Views::getView('av_portal_test');
+    $this->executeView($view, 'page_3');
+    $this->assertInstanceOf(AVPortalQuery::class, $view->query, 'Wrong query plugin used in the view.');
+
+    // By default, the view should show 10 results.
+    $this->assertCount(10, $view->result);
+
+    // Assert the first result from the mock.
+    $row = $view->result[0];
+    $this->assertEquals('I-163595', $row->ref);
+    $this->assertEquals(' ', $row->title);
+    $this->assertEquals('http://defiris.ec.streamcloud.be/findmedia/15/163595/THUMB_M_I163595INT1W_6.jpg', $row->thumbnail);
+
+    // Assert the second result from the mock.
+    $row = $view->result[1];
+    $this->assertEquals('I-163308', $row->ref);
+    $this->assertEquals(' LIVE "Subsidiarity - as a building principle of the European Union" Conference in Bregenz, Austria - Welcome, keynote speech and interviews', $row->title);
+    $this->assertContains('media/images/icons/no-thumbnail.png', $row->thumbnail);
+
+    // Assert that it works correctly with the pager.
+    $view = Views::getView('av_portal_test');
+    $view->setCurrentPage(1);
+    $this->executeView($view, 'page_3');
+
+    $this->assertCount(5, $view->result);
+    $row = $view->result[0];
+    $this->assertEquals('I-163665', $row->ref);
+    $this->assertEquals(' STOCKSHOTS EP', $row->title);
+
+    // Assert that we can filter using the custom search text filter.
+    $view = Views::getView('av_portal_test');
+    $view->setExposedInput(['search' => 'europe']);
+    $this->executeView($view, 'page_3');
+
+    $this->assertCount(10, $view->result);
+    $row = $view->result[0];
+    $this->assertEquals('I-163675', $row->ref);
+    $this->assertEquals(' Start-up Europe Award Ceremony', $row->title);
   }
 
   /**
