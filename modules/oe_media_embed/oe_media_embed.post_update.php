7fed2887a3cfaee2b1c691263848a8c40a1d5dbf
Imanol <ier@opendeusto.es>
2020-01-08 13:23:47 +0000

    OPENEUROPA-2416: Use numbering on post update method names.

--- modules/oe_media_embed/oe_media_embed.post_update.php
+++ modules/oe_media_embed/oe_media_embed.post_update.php
@@ -14,7 +14,7 @@ use Drupal\Core\Entity\Entity\EntityViewMode;
 /**
  * Enable default embed view modes.
  */
-function oe_media_embed_post_update_enable_embed_view_modes(): void {
+function oe_media_embed_post_update_00001(): void {
   $storage = new FileStorage(drupal_get_path('module', 'oe_media_embed') . '/config/post_updates/enable_embed_view_modes');
   $view_mode_values = $storage->read('core.entity_view_mode.media.oe_embed');
   if (!EntityViewMode::load($view_mode_values['id'])) {
