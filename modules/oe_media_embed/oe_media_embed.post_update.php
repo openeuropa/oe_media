bd9e2f51cb6f0482db3705223447859dd249252e
Imanol <ier@opendeusto.es>
2020-01-22 12:04:16 +0000

    OPENEUROPA-2416: Change update logic.

--- modules/oe_media_embed/oe_media_embed.post_update.php
+++ modules/oe_media_embed/oe_media_embed.post_update.php
@@ -7,47 +7,22 @@
 
 declare(strict_types = 1);
 
-use Drupal\Core\Config\FileStorage;
 use Drupal\Core\Entity\Entity\EntityViewDisplay;
-use Drupal\Core\Entity\Entity\EntityViewMode;
 
 /**
- * Enable default embed view modes.
+ * Make view modes that are already available embeddable by default.
  */
 function oe_media_embed_post_update_00001(): void {
-  $storage = new FileStorage(drupal_get_path('module', 'oe_media_embed') . '/config/post_updates/enable_embed_view_modes');
-  $view_mode_values = $storage->read('core.entity_view_mode.media.oe_embed');
-  if (!EntityViewMode::load($view_mode_values['id'])) {
-    $view_mode = EntityViewMode::create($view_mode_values);
-    $view_mode->save();
-  }
-
-  $display_modes = [
-    'core.entity_view_display.media.document.oe_embed',
-    'core.entity_view_display.media.image.oe_embed',
-    'core.entity_view_display.media.remote_video.oe_embed',
-  ];
-  // Because all the optional configuration from the other modules require
-  // the "Embed" view mode, we need to enable them here to avoid
-  // unmet dependency errors.
-  if (\Drupal::moduleHandler()->moduleExists('oe_media_avportal')) {
-    $display_modes = array_merge($display_modes, [
-      'core.entity_view_display.media.av_portal_photo.oe_embed',
-      'core.entity_view_display.media.av_portal_video.oe_embed',
-    ]);
-  }
-  if (\Drupal::moduleHandler()->moduleExists('oe_media_webtools')) {
-    $display_modes = array_merge($display_modes, [
-      'core.entity_view_display.media.webtools_chart.oe_embed',
-      'core.entity_view_display.media.webtools_map.oe_embed',
-      'core.entity_view_display.media.webtools_social_feed.oe_embed',
-    ]);
-  }
-  foreach ($display_modes as $display_id) {
-    $display_values = $storage->read($display_id);
-    if (!EntityViewDisplay::load($display_values['id'])) {
-      $display_mode = EntityViewDisplay::create($display_values);
-      $display_mode->save();
+  $available_view_display_ids = \Drupal::entityQuery('entity_view_display')
+    ->condition('targetEntityType', 'media')
+    ->condition('status', TRUE)
+    ->execute();
+  $available_view_displays = EntityViewDisplay::loadMultiple($available_view_display_ids);
+  /** @var \Drupal\Core\Entity\Display\EntityViewDisplayInterface $view_display */
+  foreach ($available_view_displays as $view_display) {
+    if ($view_display->getThirdPartySetting('oe_media_embed', 'embeddable') === NULL) {
+      $view_display->setThirdPartySetting('oe_media_embed', 'embeddable', TRUE);
+      $view_display->save();
     }
   }
 }
