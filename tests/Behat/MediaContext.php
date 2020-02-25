5ce4d69b62e401b5757263a57ec99b7361269a7c
Antonio De Marco <antonio@nuvole.org>
2020-02-25 11:57:13 +0100

    OPENEUROPA-2221: Add media creation steps to MediaContext.

--- tests/Behat/MediaContext.php
+++ tests/Behat/MediaContext.php
@@ -6,13 +6,29 @@ namespace Drupal\Tests\oe_media\Behat;
 
 use Behat\Behat\Hook\Scope\AfterScenarioScope;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Gherkin\Node\TableNode;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\file\FileInterface;
 use Drupal\media\MediaInterface;
 
 /**
  * Context to related to media testing.
  */
 class MediaContext extends RawDrupalContext {
+
+  /**
+   * Keep track of medias so they can be cleaned up.
+   *
+   * @var array
+   */
+  protected $medias = [];
+
+  /**
+   * Keep track of files so they can be cleaned up.
+   *
+   * @var array
+   */
+  protected $files = [];
 
   /**
    * The config context.
@@ -29,7 +45,7 @@ class MediaContext extends RawDrupalContext {
    *
    * @BeforeScenario
    */
-  public function gatherContexts(BeforeScenarioScope $scope) {
+  public function gatherContexts(BeforeScenarioScope $scope): void {
     $environment = $scope->getEnvironment();
     $this->configContext = $environment->getContext('Drupal\DrupalExtension\Context\ConfigContext');
   }
@@ -80,6 +96,174 @@ class MediaContext extends RawDrupalContext {
   public function visitMediaPage(string $name): void {
     $media = $this->getMediaByName($name);
     $this->visitPath($media->toUrl()->toString());
+  }
+
+  /**
+   * Creates media documents with the specified file names.
+   *
+   * Usage example:
+   *
+   * Given the following documents:
+   *   | name   | file   |
+   *   | name 1 | file 1 |
+   *   |   ...  |   ...  |
+   *
+   * @Given the following documents:
+   */
+  public function createMediaDocuments(TableNode $file_table): void {
+    // Retrieve the url table from the test scenario.
+    $files = $file_table->getColumnsHash();
+
+    foreach ($files as $file_properties) {
+      $file = $this->createFileEntity($file_properties['file']);
+
+      $media = \Drupal::service('entity_type.manager')
+        ->getStorage('media')->create([
+          'bundle' => 'document',
+          'name' => $file_properties['name'],
+          'oe_media_file' => [
+            'target_id' => (int) $file->id(),
+          ],
+          'status' => 1,
+        ]);
+
+      $media->save();
+
+      // Store for cleanup.
+      $this->medias[] = $media;
+    }
+  }
+
+  /**
+   * Creates media documents with the specified file names.
+   *
+   * Usage example:
+   *
+   * Given the following images:
+   *   | name   | file   | alt   | title   |
+   *   | name 1 | file 1 | alt 1 | title 1 |
+   *   | ...    | ...    | ...   | ...     |
+   *
+   * Properties "alt" and "title" are optional, "name" will be used if they
+   * are not provided.
+   *
+   * @Given the following images:
+   */
+  public function createMediaImages(TableNode $file_table): void {
+    // Retrieve the url table from the test scenario.
+    $files = $file_table->getColumnsHash();
+
+    foreach ($files as $file_properties) {
+      $file = $this->createFileEntity($file_properties['file']);
+
+      $media = \Drupal::service('entity_type.manager')
+        ->getStorage('media')->create([
+          'bundle' => 'image',
+          'name' => $file_properties['name'],
+          'oe_media_image' => [
+            'target_id' => (int) $file->id(),
+            'alt' => $file_properties['alt'] ?? $file_properties['name'],
+            'title' => $file_properties['title'] ?? $file_properties['name'],
+          ],
+          'status' => 1,
+        ]);
+
+      $media->save();
+
+      // Store for cleanup.
+      $this->medias[] = $media;
+    }
+  }
+
+  /**
+   * Creates media AVPortal photos with the specified URLs.
+   *
+   * Usage example:
+   *
+   * Given the following AV Portal photos:
+   *   | url   |
+   *   | url 1 |
+   *   |  ...  |
+   *
+   * @Given the following AV Portal photos:
+   */
+  public function createMediaAvPortalPhotos(TableNode $url_table): void {
+    // Retrieve the url table from the test scenario.
+    $urls = $url_table->getColumnsHash();
+
+    $pattern = '@audiovisual\.ec\.europa\.eu/(.*)/photo/(P\-.*\~2F.*)@i';
+
+    foreach ($urls as $url) {
+      $url = reset($url);
+
+      preg_match_all($pattern, $url, $matches);
+      if (empty($matches)) {
+        continue;
+      }
+
+      // Converts the slash in the photo id.
+      $photo_id = str_replace("~2F", "/", $matches[2][0]);
+
+      $media = \Drupal::service('entity_type.manager')
+        ->getStorage('media')->create([
+          'bundle' => 'av_portal_photo',
+          'oe_media_avportal_photo' => $photo_id,
+          'status' => 1,
+        ]);
+
+      $media->save();
+
+      // Store for cleanup.
+      $this->medias[] = $media;
+    }
+  }
+
+  /**
+   * Remove any created media.
+   *
+   * @AfterScenario
+   */
+  public function cleanMedias(): void {
+    if (empty($this->medias)) {
+      return;
+    }
+
+    \Drupal::entityTypeManager()->getStorage('media')->delete($this->medias);
+    $this->medias = [];
+  }
+
+  /**
+   * Remove any created files.
+   *
+   * @AfterScenario
+   */
+  public function cleanFiles(): void {
+    if (empty($this->files)) {
+      return;
+    }
+
+    \Drupal::entityTypeManager()->getStorage('file')->delete($this->files);
+    $this->files = [];
+  }
+
+  /**
+   * Create a file entity from given file and return its object.
+   *
+   * @param string $file_name
+   *   File name, relative to Mink 'files_path' location.
+   *
+   * @return \Drupal\file\FileInterface
+   *   File entity object.
+   */
+  protected function createFileEntity(string $file_name): FileInterface {
+    $file = file_save_data(file_get_contents($this->getMinkParameter('files_path') . $file_name), 'public://' . $file_name);
+    $file->setPermanent();
+    $file->save();
+
+    // Store for cleanup.
+    $this->files[] = $file;
+
+    return $file;
   }
 
   /**
