df38cbc83edbb261fffe43239ad3def8d0dc5b82
22Alexandra <beldean.ale@gmail.com>
2020-04-07 17:07:15 +0200

    OPENEUROPA-2975: Improve post update.

--- modules/oe_media_webtools/oe_media_webtools.post_update.php
+++ modules/oe_media_webtools/oe_media_webtools.post_update.php
@@ -12,15 +12,28 @@ use Drupal\field\Entity\FieldConfig;
 /**
  * Update webtools media fields description to open the wizard in new tab.
  */
-function oe_media_webtools_post_update_00001(): void {
+function oe_media_webtools_post_update_00001() {
+  $original_description = 'Enter the snippet without the script tag. Snippets can be generated in <a href="https://europa.eu/webtools/mgmt/wizard/">Webtools wizzard</a>.';
+  $new_description = 'Enter the snippet without the script tag. Snippets can be generated in <a href="https://europa.eu/webtools/mgmt/wizard/" target="_blank">Webtools wizard</a>.';
   $fields = [
     'media.webtools_chart.oe_media_webtools',
     'media.webtools_map.oe_media_webtools',
     'media.webtools_social_feed.oe_media_webtools',
   ];
+  $modified = [];
+
   foreach ($fields as $field) {
     $field_config = FieldConfig::load($field);
-    $field_config->setDescription('Enter the snippet without the script tag. Snippets can be generated in <a href="https://europa.eu/webtools/mgmt/wizard/" target="_blank">Webtools wizard</a>.');
+    // If the description has been customised by users, we don’t change it.
+    if ($original_description !== $field_config->get('description')) {
+      $modified[] = $field;
+      continue;
+    }
+    $field_config->setDescription($new_description);
     $field_config->save();
+  }
+
+  if (!empty($modified)) {
+    return 'The field description for the following fields was skipped as it was changed: ' . implode(', ', $modified);
   }
 }
