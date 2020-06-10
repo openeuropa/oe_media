e4dbea940e01132eca84fce500ed8e04cbd40f72
Francesco Sardara <francesco.sardara@ausy.be>
2020-06-10 17:45:03 +0200

    OPENEUROPA-3295: Add an iframe media source plugin.

---
+++ modules/oe_media_iframe/src/Plugin/media/Source/Iframe.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_media_iframe\Plugin\media\Source;
+
+use Drupal\media\MediaSourceBase;
+
+/**
+ * Iframe media source.
+ *
+ * @MediaSource(
+ *   id = "oe_media_iframe",
+ *   label = @Translation("Iframe"),
+ *   description = @Translation("Use iframes as source for medias."),
+ *   allowed_field_types = {"string_long"}
+ * )
+ */
+class Iframe extends MediaSourceBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMetadataAttributes() {
+    return [];
+  }
+
+}
