fc0ded35c8f0e180143ae4c5a58935c8eb3d8487
Francesco Sardara <francesco.sardara@ausy.be>
2020-06-11 10:38:37 +0200

    OPENEUROPA-3295: Add test coverage for the new media type.

---
+++ tests/Behat/FeatureContext.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_media\Behat;
+
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\RawMinkContext;
+use PHPUnit\Framework\Assert;
+
+/**
+ * Provides step definitions to interact with page elements.
+ */
+class FeatureContext extends RawMinkContext {
+
+  /**
+   * Checks that a select field has exclusively the provided options.
+   *
+   * @param string $select
+   *   The name of the select element.
+   * @param \Behat\Gherkin\Node\TableNode $table
+   *   The list of expected options.
+   *
+   * @Then the available options in the :select select should be:
+   */
+  public function assertSelectOptions(string $select, TableNode $table): void {
+    $field = $this->getSession()->getPage()->find('named', ['select', $select]);
+
+    if (empty($field)) {
+      throw new \Exception("Select field '{$select}' not found.");
+    }
+
+    $available_options = [];
+    foreach ($field->findAll('xpath', '//option') as $element) {
+      /** @var \Behat\Mink\Element\NodeElement $element */
+      $available_options[$element->getValue()] = trim($element->getText());
+    }
+    sort($available_options);
+
+    $options = $table->getColumn(0);
+    sort($options);
+
+    Assert::assertEquals($options, $available_options, "The '{$select}' select options don't match the expected ones.");
+  }
+
+}
---
+++ tests/features/video-iframe.feature
@@ -0,0 +1,22 @@
+@api
+Feature: Video iframe media.
+  In order to show remote videos
+  As a site editor
+  I want to be able to create medias with iframe as source.
+
+  @cleanup:node @cleanup:media @media-enable-standalone-url
+  Scenario: Video iframe medias can be uploaded and attached to nodes.
+    Given I am logged in as a user with the "create oe_media_demo content, create video_iframe media" permissions
+    When I go to "the Video iframe creation page"
+    Then I should see the heading "Add Video iframe"
+    And the available options in the "Ratio" select should be:
+      | 16:9 |
+      | 4:3  |
+      | 3:2  |
+      | 1:1  |
+    When I fill in "Name" with "EBS"
+    And I fill in "Iframe" with "<iframe src=\"http://web:8080/tests/fixtures/example.html\" width=\"800\" height=\"600\" frameborder=\"0\"><a href=\"#\">invalid</a></iframe><script type=\"text/javascript\">alert('no js')</script>"
+    And I press "Save"
+    Then I should see the heading "EBS"
+    # Verify that the iframe has been embedded, with all the tags except "<iframe>" stripped.
+    And the response should contain "<iframe src=\"http://web:8080/tests/fixtures/example.html\" width=\"800\" height=\"600\" frameborder=\"0\">invalid</iframe>alert('no js')"
