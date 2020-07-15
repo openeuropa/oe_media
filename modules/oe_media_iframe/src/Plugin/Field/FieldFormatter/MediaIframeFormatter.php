3db5860d5170b88c81871c906b2d28865777eb12
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2020-07-15 19:01:40 +0200

    EWPP-82: Add new field widget, update existing media source and field formatter.

--- modules/oe_media_iframe/src/Plugin/Field/FieldFormatter/MediaIframeFormatter.php
+++ modules/oe_media_iframe/src/Plugin/Field/FieldFormatter/MediaIframeFormatter.php
@@ -4,10 +4,14 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_media_iframe\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\oe_media_iframe\Plugin\media\Source\Iframe;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation for the "oe_media_iframe" formatter plugin.
@@ -21,7 +25,56 @@ use Drupal\oe_media_iframe\Plugin\media\Source\Iframe;
  *   }
  * )
  */
-class MediaIframeFormatter extends FormatterBase {
+class MediaIframeFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a StringFormatter instance.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings settings.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -48,12 +101,35 @@ class MediaIframeFormatter extends FormatterBase {
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
 
-    foreach ($items as $delta => $item) {
-      $elements[$delta] = [
-        '#markup' => $item->value,
-        '#allowed_tags' => ['iframe'],
-      ];
+    $media_type = $this->entityTypeManager->getStorage('media_type')->load($this->fieldDefinition->getTargetBundle());
+    // Add cacheable dependency from the media type.
+    $cacheable_metadata = CacheableMetadata::createFromRenderArray($elements)
+      ->addCacheableDependency($media_type);
+
+    $text_format = $media_type->getSource()->getConfiguration()['text_format'] ?? NULL;
+
+    if ($text_format) {
+      foreach ($items as $delta => $item) {
+        $elements[$delta] = [
+          '#type' => 'processed_text',
+          '#text' => $item->value,
+          '#format' => $text_format,
+        ];
+      }
+      // Add cacheable dependency from the used filter format.
+      $cacheable_metadata->addCacheableDependency($this->entityTypeManager->getStorage('filter_format')->load($text_format));
     }
+    else {
+      // Fallback in case if somehow we don't have the selected text format.
+      foreach ($items as $delta => $item) {
+        $elements[$delta] = [
+          '#markup' => $item->value,
+          '#allowed_tags' => ['iframe'],
+        ];
+      }
+    }
+
+    $cacheable_metadata->applyTo($elements);
 
     return $elements;
   }
--- modules/oe_media_iframe/src/Plugin/media/Source/Iframe.php
+++ modules/oe_media_iframe/src/Plugin/media/Source/Iframe.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_media_iframe\Plugin\media\Source;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\media\MediaSourceBase;
 
 /**
@@ -22,8 +23,39 @@ class Iframe extends MediaSourceBase {
   /**
    * {@inheritdoc}
    */
+  public function defaultConfiguration() {
+    return [
+      'text_format' => 'oe_media_iframe',
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function getMetadataAttributes() {
     return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+
+    $text_formats = [];
+    /** @var \Drupal\filter\FilterFormatInterface $filter_format */
+    foreach (filter_formats() as $filter_format) {
+      $text_formats[$filter_format->get('format')] = $filter_format->get('name');
+    }
+
+    $form['text_format'] = [
+      '#title' => $this->t('Text format'),
+      '#type' => 'select',
+      '#options' => $text_formats,
+      '#default_value' => $this->getConfiguration()['text_format'],
+      '#description' => $this->t('Pick the text format which could be used with a iframe input field.'),
+    ];
+
+    return parent::buildConfigurationForm($form, $form_state);
   }
 
 }
