41b57206664fcc02d4124d8ff67cf7d211e4ee48
22Alexandra <beldean.ale@gmail.com>
2020-07-16 17:26:21 +0200

    EWPP-84: Fix post_update.

--- modules/oe_media_iframe/oe_media_iframe.post_update.php
+++ modules/oe_media_iframe/oe_media_iframe.post_update.php
@@ -8,16 +8,47 @@
 declare(strict_types = 1);
 
 /**
- * Add thumbnail field to video iframe media.
+ * Add thumbnail field to media types using iframe source.
  */
-function oe_media_iframe_post_update_thumbnail() {
+function oe_media_iframe_post_update_00001() {
   $entity_type_manager = \Drupal::entityTypeManager();
   $media_type_storage = $entity_type_manager->getStorage('media_type');
   $iframe_types = $media_type_storage->loadByProperties(['source' => 'oe_media_iframe']);
+
+  $fields = \Drupal::service('entity_field.manager')->getFieldStorageDefinitions('media');
+  if (!isset($fields['oe_media_iframe_thumbnail']) ||
+    $fields['oe_media_iframe_thumbnail']->getTargetEntityTypeId() !== 'media') {
+    $storage = $entity_type_manager
+      ->getStorage('field_storage_config')
+      ->create([
+        'entity_type' => 'media',
+        'field_name' => 'oe_media_iframe_thumbnail',
+        'type' => 'image',
+      ]);
+    $storage->save();
+  }
+  else {
+    $storage = $fields['oe_media_iframe_thumbnail'];
+  }
+
   foreach ($iframe_types as $type) {
-    $type->getSource()->createSourceField($type);
-    // Update default form display to include the thumbnail field.
+    $field = $entity_type_manager
+      ->getStorage('field_config')
+      ->create([
+        'field_storage' => $storage,
+        'bundle' => $type->id(),
+        'label' => 'Iframe thumbnail',
+        'required' => FALSE,
+      ]);
+    $field->save();
+
+    // Update default form display to include the thumbnail after source.
     $form_display = $entity_type_manager->getStorage('entity_form_display')->load('media.' . $type->id() . '.default');
-    $type->getSource()->prepareFormDisplay($type, $form_display);
+    $source_field_name = $type->getSource()->getSourceFieldDefinition($type)->getName();
+    $source_component = $form_display->getComponent($source_field_name);
+    $thumbnail_weight = ($source_component && isset($source_component['weight'])) ? $source_component['weight'] + 1 : -50;
+    $form_display->setComponent('oe_media_iframe_thumbnail', [
+      'weight' => $thumbnail_weight,
+    ])->save();
   }
 }
