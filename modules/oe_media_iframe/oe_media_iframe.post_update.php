47097e115c41206fc3a08f2efafbe4b14ff71625
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2020-07-30 18:27:56 +0200

    EWPP-82: Update post update.

--- modules/oe_media_iframe/oe_media_iframe.post_update.php
+++ modules/oe_media_iframe/oe_media_iframe.post_update.php
@@ -57,33 +57,40 @@ function oe_media_iframe_post_update_00001(): void {
  * Incorporate text format setting into Iframe media source.
  */
 function oe_media_iframe_post_update_00002(): void {
-  $format = \Drupal::entityTypeManager()->getStorage('filter_format')->create([
-    'format' => 'oe_media_iframe',
-    'name' => 'Iframe Media',
-    'filters' => [
-      'filter_html' => [
-        'settings' => [
-          'allowed_html' => '<iframe allow allowfullscreen allowpaymentrequest csp height importance loading name referrerpolicy sandbox src srcdoc width mozallowfullscreen webkitAllowFullScreen scrolling frameborder>',
+  $format = \Drupal::entityTypeManager()->getStorage('filter_format')->load('oe_media_iframe');
+  if (!$format) {
+    $format = \Drupal::entityTypeManager()->getStorage('filter_format')->create([
+      'format' => 'oe_media_iframe',
+      'name' => 'Iframe Media',
+      'filters' => [
+        'filter_html' => [
+          'settings' => [
+            'allowed_html' => '<iframe allowfullscreen height importance loading name referrerpolicy sandbox src width mozallowfullscreen webkitAllowFullScreen scrolling frameborder>',
+          ],
+          'status' => TRUE,
         ],
-        'status' => TRUE,
       ],
-    ],
-  ]);
-  $format->save();
+    ]);
+    $format->save();
+  }
 
   $entity_form_storage = \Drupal::entityTypeManager()->getStorage('entity_form_display');
   $form_display = $entity_form_storage->load('media.video_iframe.default');
   $source_field = $form_display->getComponent('oe_media_iframe');
   if ($source_field) {
-    $source_field['type'] = 'oe_media_iframe';
+    $source_field['type'] = 'oe_media_iframe_textarea';
     $form_display->setComponent('oe_media_iframe', $source_field);
     $form_display->save();
     // Invalidate the cache of related config manually as workaround.
     \Drupal::cache('config')->delete('core.entity_form_display.media.video_iframe.default');
   }
-  $video_iframe = \Drupal::entityTypeManager()->getStorage('media_type')->load('video_iframe');
-  $settings = $video_iframe->getSource()->getConfiguration();
-  $settings['text_format'] = 'oe_media_iframe';
-  $video_iframe->set('source_configuration', $settings);
-  $video_iframe->save();
+  $media_type_storage = \Drupal::entityTypeManager()->getStorage('media_type');
+  $iframe_types = $media_type_storage->loadByProperties(['source' => 'oe_media_iframe']);
+  foreach ($iframe_types as $iframe_media_type) {
+    $settings = $iframe_media_type->getSource()->getConfiguration();
+    $settings['text_format'] = 'oe_media_iframe';
+    $iframe_media_type->set('source_configuration', $settings);
+    $iframe_media_type->save();
+  }
+
 }
