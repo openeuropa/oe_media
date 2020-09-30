d330963fd2ac51023142b268e2490cbacaa1e773
Francesco Sardara <francesco.sardara@ausy.be>
2020-09-30 16:58:55 +0200

    EWPP-292: Add test coverage for the widget.

---
+++ modules/oe_media_iframe/tests/src/Functional/IframeWidgetTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_media_iframe\Functional;
+
+use Drupal\Tests\media\Functional\MediaFunctionalTestBase;
+use Drupal\Tests\oe_media\Traits\MediaTypeCreationTrait;
+
+/**
+ * Tests the iframe widget.
+ *
+ * @group oe_media_iframe
+ */
+class IframeWidgetTest extends MediaFunctionalTestBase {
+
+  use MediaTypeCreationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'oe_media_iframe',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->createMediaType('oe_media_iframe', [
+      'id' => 'test_iframe',
+      'label' => 'Test iframe source',
+      'source' => 'oe_media_iframe',
+      'source_configuration' => [
+        'text_format' => 'plain_text',
+      ],
+    ]);
+  }
+
+  /**
+   * Tests the widget form element.
+   */
+  public function testWidgetElements(): void {
+    $this->drupalGet('/media/add/test_iframe');
+
+    // Verify that the filter tips are present just after the iframe field.
+    $tips = $this->getSession()->getPage()->findAll('css', '.form-item-field-media-oe-media-iframe-0-value + ul > li');
+    $this->assertCount(3, $tips);
+    $this->assertEquals('No HTML tags allowed.', $tips[0]->getText());
+    $this->assertEquals('Lines and paragraphs break automatically.', $tips[1]->getText());
+    $this->assertEquals('Web page addresses and email addresses turn into links automatically.', $tips[2]->getText());
+    $this->assertEquals('textarea', $this->assertSession()->fieldExists('Iframe')->getTagName());
+  }
+
+}
