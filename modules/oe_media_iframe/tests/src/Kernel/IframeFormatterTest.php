a2a3f6afb0eb14c7532da8b2db38608fbb5608c9
Francesco Sardara <francesco.sardara@ausy.be>
2020-09-30 11:23:25 +0200

    EWPP-292: Rework the formatter test to actually test its behaviour.
    Test should focus on the fact that the text format selected in the media source is applied.

--- modules/oe_media_iframe/tests/src/Kernel/IframeFormatterTest.php
+++ modules/oe_media_iframe/tests/src/Kernel/IframeFormatterTest.php
@@ -60,11 +60,12 @@ class IframeFormatterTest extends KernelTestBase {
   protected function setUp() {
     parent::setUp();
     $this->installConfig([
-      'system',
       'field',
-      'options',
-      'user',
+      'filter',
       'media',
+      'options',
+      'system',
+      'user',
       'oe_media',
       'oe_media_iframe',
     ]);
@@ -91,7 +92,7 @@ class IframeFormatterTest extends KernelTestBase {
    * Tests iframe formatter output.
    */
   public function testIframeFormatter(): void {
-    $value = '<iframe src="http://web:8080/tests/fixtures/example.html" invalid-attribute="with value" width="800" height="600" frameborder="0" allow allowfullscreen allowpaymentrequest csp importance loading name referrerpolicy sandbox srcdoc mozallowfullscreen webkitAllowFullScreen scrolling accesskey autocapitalize class contenteditable data-test data-test2 dir draggable dropzone exportparts hidden id inputmode is itemid itemprop itemref itemscope itemtype lang part slot spellcheck style tabindex title translate><a href="#">invalid</a></iframe><script type="text/javascript">alert(\'no js\')</script>';
+    $value = '<iframe src="http://web:8080/tests/fixtures/example.html" invalid-attribute="with value" width="800" height="600" frameborder="0" name="my-iframe">Iframes not available.</iframe><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>';
 
     $entity = \Drupal::entityTypeManager()->getStorage('media')->create([
       'bundle' => $this->mediaType->id(),
@@ -100,10 +101,21 @@ class IframeFormatterTest extends KernelTestBase {
     ]);
     $entity->{$this->fieldName}->value = $value;
 
-    // Verify that all allowed attributes is present and disallowed is removed.
+    // Verify that the oe_media_iframe format has been applied to the output.
     $this->renderEntityFields($entity, $this->display);
     $this->assertNoRaw($value);
-    $this->assertRaw('<iframe src="http://web:8080/tests/fixtures/example.html" width="800" height="600" frameborder="0" allowfullscreen="" importance="" loading="" referrerpolicy="" sandbox="" mozallowfullscreen="" webkitallowfullscreen="" scrolling="" lang="" xml:lang="" xml:lang="">invalid</iframe>');
+    $this->assertRaw('<iframe src="http://web:8080/tests/fixtures/example.html" width="800" height="600" frameborder="0">Iframes not available.</iframe>');
+
+    // Change the media source text format to plain_text.
+    $configuration = $this->mediaType->get('source_configuration');
+    $configuration['text_format'] = 'plain_text';
+    $this->mediaType->set('source_configuration', $configuration)->save();
+
+    // Verify that the newly selected format is now applied to the output of
+    // the formatter.
+    $this->renderEntityFields($entity, $this->display);
+    $this->assertNoRaw($value);
+    $this->assertRaw('<p>&lt;iframe src=&quot;<a href="http://web:8080/tests/fixtures/example.html&amp;quot">http://web:8080/tests/fixtures/example.html&amp;quot</a>; invalid-attribute=&quot;with value&quot; width=&quot;800&quot; height=&quot;600&quot; frameborder=&quot;0&quot; name=&quot;my-iframe&quot;&gt;Iframes not available.&lt;/iframe&gt;&lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit&lt;/p&gt;</p>');
   }
 
   /**
@@ -113,14 +125,10 @@ class IframeFormatterTest extends KernelTestBase {
    *   The entity object with attached fields to render.
    * @param \Drupal\Core\Entity\Display\EntityViewDisplayInterface $display
    *   The display to render the fields in.
-   *
-   * @return string
-   *   The rendered entity fields.
    */
-  protected function renderEntityFields(FieldableEntityInterface $entity, EntityViewDisplayInterface $display): string {
+  protected function renderEntityFields(FieldableEntityInterface $entity, EntityViewDisplayInterface $display): void {
     $content = $display->build($entity);
-    $content = $this->render($content);
-    return $content;
+    $this->render($content);
   }
 
 }
